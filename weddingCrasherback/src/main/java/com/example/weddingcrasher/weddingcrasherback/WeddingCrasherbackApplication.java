fbcf8e58d3ba04a8b786350cc4f08ce6a8c8f717
Johnny <aguguaj@hotmail.com>
2022-02-24 10:35:59 -0600

    project init

---
+++ weddingCrasherback/src/main/java/com/example/weddingcrasher/weddingcrasherback/WeddingCrasherbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.weddingcrasher.weddingcrasherback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingCrasherbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingCrasherbackApplication.class, args);
+    }
+
+}
---
+++ weddingCrasherback/src/test/java/com/example/weddingcrasher/weddingcrasherback/WeddingCrasherbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weddingcrasher.weddingcrasherback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingCrasherbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
